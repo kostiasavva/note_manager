5864f5a6f51cd744e3b50e3f0960eeb6697834c1
kostiasavva <k.savva@mail.ru>
2024-12-23 12:25:30 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,15 @@
+user_name = 'Петя Васичкин'
+print('Имя пользователя: ' + user_name)
+title = 'Автомобиль'
+print('Заголовок заметки: ' + title)
+content = 'Поменять шаровую опору'
+print('Описание заметки: ' + content)
+status = False
+print('Статус заметки: ' + str(status))
+import datetime
+created_date = datetime.date(2024, 12, 23)
+print('Дата создания заметки: ' + str(created_date.day) + '-' + str(created_date.month) + '-' + str(created_date.year))
+issue_date = datetime.date(2024, 12, 29)
+print('Дата истечения заметки: ' + str(created_date.day) + '-' + str(created_date.month) + '-' + str(created_date.year))
+# '%B %d, %Y'
+# datetime.date.strftime
