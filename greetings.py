cfb69b0e2a2d0573ade7b6e24330aaa93b4f8cc3
kostiasavva <k.savva@mail.ru>
2024-12-24 18:21:45 +0300

    Update greetings.py

--- greetings.py
+++ greetings.py
@@ -10,4 +10,4 @@ import datetime
 created_date = datetime.date(2024, 12, 23)
 print('Дата создания заметки: ' + str(created_date.day) + '-' + str(created_date.month) + '-' + str(created_date.year))
 issue_date = datetime.date(2024, 12, 29)
-print('Дата истечения заметки: ' + str(created_date.day) + '-' + str(created_date.month) + '-' + str(created_date.year))
+print('Дата истечения заметки: ' + str(issue_date.day) + '-' + str(issue_date.month) + '-' + str(issue_date.year))
