f3bd244fd70056e40c471e3ca253eb703f9e9a19
kostiasavva <k.savva@mail.ru>
2025-01-21 15:07:18 +0300

    Update final.py
    Реализовал введение даты через функции .strptime
    а вывод, соответственно, через .strftime

--- final.py
+++ final.py
@@ -1,96 +1,30 @@
-heading = ['Имя пользователя: ', 'Заголовок заметки: ', 'Описание заметки: ', 'некий СПИСОК', 'Дата создания: ', 'Завершить до: ', 'Статус: ']
+heading = ['Имя пользователя: ', 'Заголовок заметки: ', 'Описание заметки: ', 'некий СПИСОК', 'Дата создания (ДД-ММ): ', 'Завершить до (ДД-ММ): ', 'Статус: ']
 content = ['некая Строка', 'некая Строка', 'некая Строка', 'некиий СПИСОК', 'некаяя ДАТА', 'некая ДАТА', 'ДА/НЕТ']
 
 content[0] = input('Введите Ваше имя: ')
 content[1] = input('Введите заголовок заметки: ')
 content[2] = input('Опишите суть заметки: ')
 
-import datetime
+from datetime import datetime
 
 # Ввод даты оформления заметки
 while True:
-    while True:
-        created_date_str_day = input('Введите сегодняшшее число: ')
-        try: created_date_day = int(created_date_str_day)
-        except ValueError:
-            print('Введите корректное число!')
-            continue
-        else:
-            if created_date_day > 31 or created_date_day < 1:
-                print('Введите корректное число!')
-                continue
-        break
-    while True:
-        created_date_str_month = input('Введите текущий месяц цифрами: ')
-        try: created_date_month = int(created_date_str_month)
-        except ValueError:
-            print('Введите корректный номер месяца!')
-            continue
-        else:
-            if created_date_month > 12 or created_date_month < 1:
-                print('Введите корректный номер месяца!')
-                continue
-        break
-    while True:
-        created_date_str_year = input('Введите текущий год: ')
-        try: created_date_year = int(created_date_str_year)
-        except ValueError:
-            print('Введите год корректно!')
-            continue
-        else:
-            if created_date_year > 2050 or created_date_year < 2024:
-                print('Введите год корректно!')
-                continue
-        break
-    try: # Это делается для выяления возможной ошибки ввода не существующего 31 числа (29, 30)
-        created_date = datetime.date(created_date_year, created_date_month, created_date_day)
+    created_date_str = input('Введите сегодняшшее число в формате ДД.ММ.ГГГГ, где ММ - цифрами: ')
+    try: created_date = datetime.strptime(created_date_str, '%d.%m.%Y')
     except ValueError:
-        print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nВведите заново КОРРЕКТНУЮ дату!\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+        print('Введите корректную дату!')
         continue
-    else: break
+    break
 content[4] = created_date
 
 # Ввод даты окончания срока выполнения
 while True:
-    while True:
-        issue_date_str_day = input('Введите число даты истечения срока выполнения: ')
-        try: issue_date_day = int(issue_date_str_day)
-        except ValueError:
-            print('Введите корректное число!')
-            continue
-        else:
-            if issue_date_day > 31 or issue_date_day < 1:
-                print('Введите корректное число!')
-                continue
-        break
-    while True:
-        issue_date_str_month = input('Введите месяц истечения срока цифрами: ')
-        try: issue_date_month = int(issue_date_str_month)
-        except ValueError:
-            print('Введите корректный номер месяца!')
-            continue
-        else:
-            if issue_date_month > 12 or issue_date_month < 1:
-                print('Введите корректный номер месяца!')
-                continue
-        break
-    while True:
-        issue_date_str_year = input('Введите год истечения срока: ')
-        try: issue_date_year = int(issue_date_str_year)
-        except ValueError:
-            print('Введите год корректно!')
-            continue
-        else:
-            if issue_date_year > 2050 or issue_date_year < 2024:
-                print('Введите год корректно!')
-                continue
-        break
-    try: # Это делается для выяления возможной ошибки ввода не существующего 31 числа (29, 30)
-        issue_date = datetime.date(issue_date_year, issue_date_month, issue_date_day)
+    issue_date_str = input('Введите дату истечения срока выполнения в формате ДД.ММ.ГГГГ: ')
+    try: issue_date = datetime.strptime(issue_date_str, '%d.%m.%Y')
     except ValueError:
-        print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nВведите заново КОРРЕКТНУЮ дату!\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+        print('Введите корректное число!')
         continue
-    else: break
+    break
 content[5] = issue_date
 
 n = input('Введите нужное количество дополнителых полей: ')
@@ -115,12 +49,13 @@ print(heading[2] + content[2].capitalize())
 for i in range(n):
     print('  ' + str(i + 1) +  '. ' + heading[3][i].capitalize() + ': ' + content[3][i].capitalize())
 
-print(heading[4] + str(created_date.day) + '-' + str(created_date.month))
-print(heading[5] + str(issue_date.day) + '-' + str(issue_date.month))
+print(heading[4] + created_date.strftime('%d-%m'))
+print(heading[5] + issue_date.strftime('%d-%m'))
 
 # Проверка "Срок истёк?"
 status = False
-if datetime.date.today() > issue_date:
+
+if datetime.today() > issue_date:
     status = True
 if status == False:
     content[6] = 'Заметка в работе'
