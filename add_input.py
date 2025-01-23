b511fe68960288872cbbc358bfd165faf37f3605
kostiasavva <k.savva@mail.ru>
2025-01-23 16:51:49 +0300

    Update add_input.py
    Исправил ввод-вывод даты. Методы strptime и strftime

--- add_input.py
+++ add_input.py
@@ -2,101 +2,34 @@ user_name = input('Введите Ваше имя: ')
 title = input('Введите заголовок заметки: ')
 content = input('Опишите суть заметки: ')
 
-import datetime
+from datetime import datetime
 
 # Ввод даты оформления заметки
 while True:
-    while True:
-        created_date_str_day = input('Введите сегодняшшее число: ')
-        try: created_date_day = int(created_date_str_day)
-        except ValueError:
-            print('Введите корректное число!')
-            continue
-        else:
-            if created_date_day > 31 or created_date_day < 1:
-                print('Введите корректное число!')
-                continue
-        break
-    while True:
-        created_date_str_month = input('Введите текущий месяц цифрами: ')
-        try: created_date_month = int(created_date_str_month)
-        except ValueError:
-            print('Введите корректный номер месяца!')
-            continue
-        else:
-            if created_date_month > 12 or created_date_month < 1:
-                print('Введите корректный номер месяца!')
-                continue
-        break
-    while True:
-        created_date_str_year = input('Введите текущий год: ')
-        try: created_date_year = int(created_date_str_year)
-        except ValueError:
-            print('Введите год корректно!')
-            continue
-        else:
-            if created_date_year > 2050 or created_date_year < 2024:
-                print('Введите год корректно!')
-                continue
-        break
-    try:
-        created_date = datetime.date(created_date_year, created_date_month, created_date_day)
+    created_date_str = input('Введите сегодняшшее число в формате "ДД.ММ.ГГГГ": ')
+    try: created_date = datetime.strptime(created_date_str, '%d.%m.%Y')
     except ValueError:
-        print('~~~~~~~~~~~~~~~~~~~~~~~~~~Ё~~~~\nВведите заново КОРРЕКТНУЮ дату!\n~~~Ё~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+        print('Введите корректное число! День, месяц, год.')
         continue
     else: break
-
 # Ввод даты окончания срока выполнения
 while True:
-    while True:
-        issue_date_str_day = input('Введите число даты истечения срока выполнения: ')
-        try: issue_date_day = int(issue_date_str_day)
-        except ValueError:
-            print('Введите корректное число!')
-            continue
-        else:
-            if issue_date_day > 31 or issue_date_day < 1:
-                print('Введите корректное число!')
-                continue
-        break
-    while True:
-        issue_date_str_month = input('Введите месяц истечения срока цифрами: ')
-        try: issue_date_month = int(issue_date_str_month)
-        except ValueError:
-            print('Введите корректный номер месяца!')
-            continue
-        else:
-            if issue_date_month > 12 or issue_date_month < 1:
-                print('Введите корректный номер месяца!')
-                continue
-        break
-    while True:
-        issue_date_str_year = input('Введите год истечения срока: ')
-        try: issue_date_year = int(issue_date_str_year)
-        except ValueError:
-            print('Введите год корректно!')
-            continue
-        else:
-            if issue_date_year > 2050 or issue_date_year < 2024:
-                print('Введите год корректно!')
-                continue
-        break
-    try:
-        issue_date = datetime.date(issue_date_year, issue_date_month, issue_date_day)
+    issue_date_str = input('Введите дату истечения срока выполненияв формате "ДД.ММ.ГГГГ": ')
+    try: issue_date = datetime.strptime(issue_date_str, '%d.%m.%Y')
     except ValueError:
-        print('~~~~~~~~~~~~~~~~~~~~~~~~~~~Ё~~~\nВведите заново КОРРЕКТНУЮ дату!\n~~~~Ё~~~~~~~~~~~~~~~~~~~~~~~~~~')
+        print('Введите корректное число! День, Месяц, Год.')
         continue
     else: break
 print('========================================================')
 print('Имя пользователя: ' + user_name)
 print('Заголовок заметки: ' + title)
 print('Описание заметки: ' + content)
-print('Дата создания: ' + str(created_date.day) + '-' + str(created_date.month))
-print('Завершить до: ' + str(issue_date.day) + '-' + str(issue_date.month))
+print('Дата создания: ' + created_date.strftime('%d-%m'))
+print('Завершить до: ' + issue_date.strftime('%d-%m'))
 
 # Проверка "Срок истёк?"
 status = False
-if datetime.date.today() > issue_date:
+if datetime.today() > issue_date:
     status = True
 if status == False:
     status_expired = 'Заметка в работе'
